0efae4a26fdda84bc293aee8f1fc010c1d43c095
crislav10 <cristianlav10@gmail.com>
2021-09-05 17:10:38 -0500

    base de datos SQLite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'rr6i9tLX7i9t',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
