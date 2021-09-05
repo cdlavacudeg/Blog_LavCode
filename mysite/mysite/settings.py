e2230dc95deebe3fbdc3906fa6ce6d340e33e4f4
crislav10 <cristianlav10@gmail.com>
2021-09-05 16:35:10 -0500

    Se cambia de base de datos a PostgreSQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'rr6i9tLX7i9t',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
