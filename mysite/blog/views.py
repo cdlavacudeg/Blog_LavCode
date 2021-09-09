aa768cb019566cd53bb93bdb3fb422b1b0b41750
crislav10 <cristianlav10@gmail.com>
2021-09-09 18:02:20 -0500

    Se agrega paginación a home

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,7 +11,7 @@ class PostDetailView(DetailView):
         #post=Blog.objects.filter(~Q(id=post_of_id.id)).order_by('-creacion_en')[:3]
         #https://docs.djangoproject.com/en/3.2/ref/models/querysets/#queryset-api-reference
         post=Blog.objects.exclude(id=post_of_id.id).order_by('-creacion_en')[:3]
-        recent_list=Blog.objects.all().order_by('-modificado_en')
+        recent_list=Blog.objects.all().order_by('-modificado_en')[:5]
         cate=Categoria.objects.all().order_by('nombre')
         
         context={'blog_list':post,'post':post_of_id,'recent_post':recent_list,'categorias':cate,'id_post':post_of_id.id}
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,15 +1,25 @@
+from django.core import paginator
 from django.shortcuts import render
 from django.urls import reverse
 from blog.models import Blog,Autor,Categoria
 from django.views import View
-
+from django.core.paginator import Paginator
 # Create your views here.
 class HomeView(View):
-    
+        
     def get(self,request):
-        post=Blog.objects.all().order_by('-creacion_en')[:10]
+        
         recent_list=Blog.objects.all().order_by('-modificado_en')[:5]
         cate=Categoria.objects.all().order_by('nombre')
-        ctx={'blog_list':post,'recent_post':recent_list,'categorias':cate}
+        post=Blog.objects.all().order_by('-creacion_en')
+        
+        #Paginador de Django https://docs.djangoproject.com/en/3.2/ref/paginator/
+        paginator=Paginator(post,10)
+        page_number=request.GET.get('page')
+        page_obj=paginator.get_page(page_number)
+
+        ctx={'blog_list':page_obj,'recent_post':recent_list,'categorias':cate}
+
+
         return render(request,'home/home_list.html',ctx)
 
