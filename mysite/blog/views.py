bce84b70e122bd801d1016d119cb7599bd2082aa
crislav10 <cristianlav10@gmail.com>
2021-09-11 15:47:33 -0500

    Se agrega funcionalidad de busqueda y habilitan links de categorias

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Blog,Categoria
+from home.views import HomeView
+from django.core.paginator import Paginator
 #from django.db.models import Q
 # Create your views here.
-class PostDetailView(DetailView):
-    model=Blog
+class PostDetailView(HomeView):
+    
     template_name="blog/post_detail.html"
+
     def get(self,request,pk):
         post_of_id=Blog.objects.get(id=pk)
+        self.template_name="blog/post_detail.html"
+        context={}
+        strval=request.GET.get('search',False)
+        if strval:
+            post,strval=super().search_func(request)
+            self.template_name="home/home_list.html"
+            paginator=Paginator(post,10)
+            page_number=request.GET.get('page')
+            post=paginator.get_page(page_number)
+
+        else:
+            post=Blog.objects.exclude(id=post_of_id.id).order_by('-creacion_en')[:3]
+
         #post=Blog.objects.filter(~Q(id=post_of_id.id)).order_by('-creacion_en')[:3]
         #https://docs.djangoproject.com/en/3.2/ref/models/querysets/#queryset-api-reference
-        post=Blog.objects.exclude(id=post_of_id.id).order_by('-creacion_en')[:3]
+        
         recent_list=Blog.objects.all().order_by('-modificado_en')[:5]
         cate=Categoria.objects.all().order_by('nombre')
         
-        context={'blog_list':post,'post':post_of_id,'recent_post':recent_list,'categorias':cate,'id_post':post_of_id.id}
+        context={'blog_list':post,'post':post_of_id,'recent_post':recent_list,'categorias':cate,'search':strval}
         return render(request,self.template_name,context)
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,22 +4,43 @@ from django.urls import reverse
 from blog.models import Blog,Autor,Categoria
 from django.views import View
 from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 class HomeView(View):
+    template_name='home/home_list.html'
         
     def get(self,request):
         
         recent_list=Blog.objects.all().order_by('-modificado_en')[:5]
         cate=Categoria.objects.all().order_by('nombre')
-        post=Blog.objects.all().order_by('-creacion_en')
-        
+        post,search_val=self.search_func(request)
         #Paginador de Django https://docs.djangoproject.com/en/3.2/ref/paginator/
         paginator=Paginator(post,10)
         page_number=request.GET.get('page')
         page_obj=paginator.get_page(page_number)
 
-        ctx={'blog_list':page_obj,'recent_post':recent_list,'categorias':cate}
+        ctx={'blog_list':page_obj,'recent_post':recent_list,'categorias':cate,'search':search_val}
 
 
-        return render(request,'home/home_list.html',ctx)
+        return render(request,self.template_name,ctx)
+
+    def search_func(self,request):
+
+        strval=request.GET.get('search',False)
+        if strval :
+            # Simple title-only search
+            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            # __icontains for case-insensitive search
+            query = Q(titulo__icontains=strval)
+            #query.add(Q(contenido__icontains=strval), Q.OR)
+            blog_list = Blog.objects.filter(query).select_related().order_by('-creacion_en')
+        else :
+            blog_list = Blog.objects.all().order_by('-creacion_en')
+
+        return blog_list,strval
+
+
+    
 
