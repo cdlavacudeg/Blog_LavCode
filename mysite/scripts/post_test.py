f0050395dada5760e3d04965d8f9fd9d55d32f07
crislav10 <cristianlav10@gmail.com>
2022-01-10 17:03:14 -0500

    Deploy pythonanywhere

--- mysite/scripts/post_test.py
+++ mysite/scripts/post_test.py
@@ -2,16 +2,13 @@ from blog.models import Blog,Autor,Categoria
 
 #python manage.py runscript post_test.py
 def run():
-    titulo="Post Prueba"
+    titulo="Post Prueba "
     descripcion="Prueba insersion con Scripts"
-    imagen="https://i0.wp.com/unaaldia.hispasec.com/wp-content/uploads/2019/06/django.png?fit=1200%2C800&ssl=1"
+    imagen="https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2018/01/imagen-aleatoria-destacada.png?fit=666%2C370&quality=100&strip=all&ssl=1"
     autor=Autor.objects.get(id=1)
-    categoria=Categoria.objects.get(id=3)
+    categoria=Categoria.objects.get(id=6)
     contenido="<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Nam, cumque ea totam quisquam odit voluptatem in veniam animi, omnis facere iure, sequi officia exercitationem nesciunt eaque consequatur consectetur nemo eveniet</p>"
-    for j in range(100,500):
-        print(j)
-        
-        
+    for j in range(5,45):
         post_new=Blog(titulo=titulo+str(j),descripcion=descripcion,imagen=imagen,autor=autor,contenido=contenido)
         post_new.save()
         post_new.categoria.add(categoria)
